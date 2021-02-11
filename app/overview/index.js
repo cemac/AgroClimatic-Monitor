d633f70b8afdfef2460b746f8ca73276cd4dc7b2
Daniel Ellis <daniel.ellis.research@gmail.com>
2021-02-11 03:12:02 +0000

    app demo clean

--- app/overview/index.js
+++ app/overview/index.js
@@ -21,8 +21,8 @@ var map = L.map("lmap", {
 });
 
 var w = L.tileLayer(
-    //'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
-    "https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png",
+    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+    // "https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png",
     {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://carto.com/attributions">CARTO</a>|CEMAC',
         subdomains: "abcd",
