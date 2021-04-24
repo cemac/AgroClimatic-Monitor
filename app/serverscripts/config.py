f36c5115a531994ea5bcc4f5437d1fa5ac7cdee7
Dan Ellis <d.ellis@leeds.ac.uk>
2021-04-24 23:13:16 +0100

    Update config.py

--- app/serverscripts/config.py
+++ app/serverscripts/config.py
@@ -16,7 +16,7 @@ extensions = set(['txt', 'pdf', 'image/png', 'image/tiff','image/gtiff'])
 ### if change set in main/config.py too
 STORAGE = __file__.replace('app/serverscripts/config.py','uolstorage/Data/') #symbolic link in main repo level 1
 PROCESSED = __file__.replace('app/serverscripts/config.py','processed/')
-DATA = __file__.replace('app/serverscripts/config.py',STORAGE+'data/')
+DATA = STORAGE+'data/' #__file__.replace('app/serverscripts/config.py',STORAGE+'data/')
 STAGING = STORAGE+'upload'
 db_loc = os.path.join(__file__.replace('app/serverscripts/config.py','app/'), 'upload.db')
 
