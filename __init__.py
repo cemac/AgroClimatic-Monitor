acff43f136a9807801d8bc56ce60147d9a8eae27
Dan Ellis <d.ellis@leeds.ac.uk>
2021-04-25 18:14:49 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+'''
+The the parsing scripts for agroclimatic 
+
+'''
+
+__author__ = 'D.Ellis'
+__github__='wolfiex
+__organisation__= 'CEMAC'
+__contact__='d.ellis-A-T-leeds.ac.uk'
+
+all=['each_h5']
