8d4b97ab0a20bde81b2121621009d75694189ce7
Dan Ellis <d.ellis@leeds.ac.uk>
2021-06-27 18:05:22 +0100

    Update data_brows_text_en_uk.py

--- app/about/data_brows_text_en_uk.py
+++ app/about/data_brows_text_en_uk.py
@@ -16,11 +16,3 @@ municipality. A time series avarage can also be downloaded for any index and mun
 by selecting an indiviual item. 
 
  '''
-
-data_brows_textbox2_title = ''' Indices Interpretation '''
-
-data_brows_textbox2_text = ''' 
-Table 1. Drought classification for SPI, VHI, RZSM and IDI  
-<table border="1" class="table table-bordered dataTable">\n  <thead>\n    <tr style="text-align: right;">\n      <th></th>\n      <th>SPI</th>\n      <th>VHI</th>\n      <th>RZSM</th>\n      <th>Drought Classification</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n      <th>0</th>\n      <td>&gt; -0.5</td>\n      <td>&gt; 40</td>\n      <td>? 30</td>\n      <td>Normal</td>\n    </tr>\n    <tr>\n      <th>1</th>\n      <td>-0.5 to -0.8</td>\n      <td>30 to 40</td>\n      <td>20 to 30</td>\n      <td>Abnormally Dry</td>\n    </tr>\n    <tr>\n      <th>2</th>\n      <td>-0.8 to -1.3</td>\n      <td>20 to 30</td>\n      <td>11 to 20</td>\n      <td>Moderate Drought</td>\n    </tr>\n    <tr>\n      <th>3</th>\n      <td>-1.3 to -1.6</td>\n      <td>12 to 20</td>\n      <td>6 to 11</td>\n      <td>Severe Drought</td>\n    </tr>\n    <tr>\n      <th>4</th>\n      <td>-1.6 to -2.0</td>\n      <td>6 to 12</td>\n      <td>3 to 6</td>\n      <td>Extreme Drought</td>\n    </tr>\n    <tr>\n      <th>5</th>\n      <td>&gt; -2.0</td>\n      <td>&lt; 6</td>\n      <td>&lt; 3</td>\n      <td>Exceptional Drought</td>\n    </tr>\n  </tbody>\n</table>
- '''
-
