397294c7f0520a93aab5fc0796588790488d79a8
cemachelen <h.l.burns@leeds.ac.uk>
2021-09-23 14:05:16 +0100

    :wrench: add startupscript.

---
+++ startupscript.sh
@@ -0,0 +1,20 @@
+# The official startup script for AgroCliMon
+# Author: D.Ellis, H burns
+# Maintainer: CEMAC
+# Institution: University of Leeds.
+
+
+
+# lets kill any running screen instances:
+screen -XS climate quit;
+screen -wipe;
+pkill -9 screen;
+
+
+# create a new screen session named "climate" in detached mode, activate the conda env and execute the app file.
+
+cd ~/ && screen -dmS climate sh -c 'cd NewVersion/AgroClimatic-Monitor/ && pipenv shell && cd app &&  gunicorn --bind 0.0.0.0:5000 wsgi:app
+ 2>&1 | tee ~/
+agr_app_logfile.log; exec bash'
+
+screen -ls
