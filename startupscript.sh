1b9f526fdb8b142624040ec621dcca546d9a2feb
cemachelen <h.l.burns@leeds.ac.uk>
2021-09-23 15:40:19 +0100

    :pencil2: remove formatting from command

--- startupscript.sh
+++ startupscript.sh
@@ -13,7 +13,6 @@ pkill -9 screen;
 
 # create a new screen session named "climate" in detached mode, activate the conda env and execute the app file.
 
-cd ~/ && screen -dmS climate sh -c 'cd AgroClimatic-Monitor/ && ~/miniconda3/bin/pipenv shell && cd app &&  gunicorn  -b 127.0.0.1:57263 --access-logfile=/home/earagc/logs/app/access.log --error-logfile=/home/earagc/logs/app/error.log --access-logformat='%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s"
-%(s)s %(b)s "%(f)s" "%(a)s"' wsgi:app ; exec bash'
+cd ~/ && screen -dmS climate sh -c 'cd AgroClimatic-Monitor/ && ~/miniconda3/bin/pipenv shell && cd app &&  gunicorn  -b 127.0.0.1:57263 --access-logfile=/home/earagc/logs/app/access.log --error-logfile=/home/earagc/logs/app/error.log wsgi:app ; exec bash'
 
 screen -ls
