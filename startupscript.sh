f8157dea3b3bb232cd597449871c1d1354bafc83
cemachelen <h.l.burns@leeds.ac.uk>
2021-09-23 14:14:20 +0100

    :pencil2: edit path

--- startupscript.sh
+++ startupscript.sh
@@ -13,7 +13,7 @@ pkill -9 screen;
 
 # create a new screen session named "climate" in detached mode, activate the conda env and execute the app file.
 
-cd ~/ && screen -dmS climate sh -c 'cd AgroClimatic-Monitor/ && miniconda3/bin/pipenv shell && cd app &&  gunicorn --bind 0.0.0.0:5000 wsgi:app
+cd ~/ && screen -dmS climate sh -c 'cd AgroClimatic-Monitor/ && ~/miniconda3/bin/pipenv shell && cd app &&  gunicorn --bind 0.0.0.0:5000 wsgi:app
  2>&1 | tee ~/agr_app_logfile.log; exec bash'
 
 screen -ls
