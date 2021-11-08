1a3b8b2195bbe4b92b2b6687673155e23d3494c3
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-08 10:47:57 +0000

    :wrench: try a different method to activate env

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,8 +1,13 @@
-activator = '/var/www/agro-python/bin//activate_this.py'  
-with open(activator) as f:
-    exec(f.read(), {'__file__': activator})
+#activator = '/var/www/agro-python/bin/activate_this.py'  
+#with open(activator) as f:
+#    exec(f.read(), {'__file__': activator})
+activate_this = '/var/www/agro-python/bin/activate_this.py'
+
+with open(activate_this) as file_:
+        exec(file_.read(), dict(__file__=activate_this))
 import sys
 sys.path.insert(0, '/var/www/AgroClimatic-Monitor/app')
+
 from app import app
 if __name__ == "__main__":
     app.run(debug=True)
