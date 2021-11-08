9942b5e806cdef2de3d3bdc1eb68e66f4ccfd2c1
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-08 14:55:10 +0000

    :light_bulb: :hammer_and_wrench: altered import and docs

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,10 +1,12 @@
+# WSGI script for mod_wgsi to use
+# Step one activate python environment via bespoke activate_this.py (supplied in repo)
 activate_this = '/var/www/agro-python/bin/activate_this.py'
 
 with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
+# Tell mod_wsgi the root dir of the app        
 import sys
 sys.path.insert(0, '/var/www/AgroClimatic-Monitor/')
 
-from app import app
-if __name__ == "__main__":
-    app.run(debug=True)
+# The standard wgsi import
+from app import app as application
