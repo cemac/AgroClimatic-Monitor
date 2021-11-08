480315c9c8c8351eccd085e66702fb97978b1e4b
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-08 11:26:51 +0000

    :hamme_and_wrench: tweak

--- activate_this.py
+++ activate_this.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# taken from https://github.com/pypa/virtualenv
+# taken from https://github.com/pypa/virtualenv and modified
 """Activate virtualenv for current interpreter:
 Use exec(open(this_file).read(), {'__file__': this_file}).
 This can be used when you must use an existing Python interpreter, not the virtualenv bin/python.
@@ -15,6 +15,7 @@ except NameError:
 
 bin_dir = os.path.dirname(abs_file)
 base = bin_dir[: -len("__BIN_NAME__") - 1]  # strip away the bin part from the __file__, plus the path separator
+base_dir = bin_dir[: -len("__BIN_NAME__") ]  # strip away the bin part from the __file__, 
 
 # prepend bin to PATH (this file is inside the bin directory)
 os.environ["PATH"] = os.pathsep.join([bin_dir] + os.environ.get("PATH", "").split(os.pathsep))
@@ -22,8 +23,8 @@ os.environ["VIRTUAL_ENV"] = base  # virtual env is right above bin directory
 
 # add the virtual environments libraries to the host python import mechanism
 prev_length = len(sys.path)
-for lib in "__LIB_FOLDERS__".split(os.pathsep):
-    path = os.path.realpath(os.path.join(bin_dir, lib))
+for lib in "lib/python3.8/site-packages/".split(os.pathsep):
+    path = os.path.realpath(os.path.join(base_dir, lib))
     print(path)
     site.addsitedir(path)
 sys.path[:] = sys.path[prev_length:] + sys.path[0:prev_length]
