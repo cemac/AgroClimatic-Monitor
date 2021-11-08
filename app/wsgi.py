43f468aabbb6b182757fd48ff8e65758cc5d47f0
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-08 11:47:11 +0000

    :hammer_and_wrench: about not found
    try adding in whole folder to path

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,11 +1,9 @@
-#activator = '/var/www/agro-python/bin/activate_this.py'  
-#with open(activator) as f:
-#    exec(f.read(), {'__file__': activator})
 activate_this = '/var/www/agro-python/bin/activate_this.py'
 
 with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
 import sys
+sys.path.insert(0, '/var/www/AgroClimatic-Monitor/')
 sys.path.insert(0, '/var/www/AgroClimatic-Monitor/app')
 
 from app import app
