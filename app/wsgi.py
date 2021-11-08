a25b4c1b6ee3e7a1dad5a250d8bb632d228f1fba
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-08 10:17:53 +0000

    :wrench: edited wsgi.py to load venv

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,3 +1,8 @@
+activator = '/var/www/agro-python/bin//activate_this.py'  
+with open(activator) as f:
+    exec(f.read(), {'__file__': activator})
+import sys
+sys.path.insert(0, '/var/www/AgroClimatic-Monitor/app')
 from app import app
 if __name__ == "__main__":
     app.run(debug=True)
