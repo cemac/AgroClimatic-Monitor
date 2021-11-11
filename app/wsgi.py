f98d22560b8131ee48a23de272c7ed0115fd461a
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-11 15:53:29 +0000

    :pencil2: forgot prev_sys_path var

--- app/wsgi.py
+++ app/wsgi.py
@@ -10,6 +10,9 @@ import site
 # Calculate path to site-packages directory.
 python_version = '.'.join(map(str, sys.version_info[:2]))
 site_packages = python_home + '/lib/python%s/site-packages' % python_version
+# Remember original sys.path.
+
+prev_sys_path = list(sys.path)
 # Add the site-packages directory.
 site.addsitedir(site_packages)
 
