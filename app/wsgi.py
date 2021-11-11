c06d4d320c01b06265f97d9865a5ef3feae541cb
Helen Burns <H.L.Burns@leeds.ac.uk>
2021-11-11 15:32:43 +0000

    :hammer_and_wrench: still not picking up venv libs

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,9 +1,12 @@
 # WSGI script for mod_wgsi to use
 # Step one activate python environment via bespoke activate_this.py (supplied in repo)
-activate_this = '/var/www/agro-python/bin/activate_this.py'
-
-with open(activate_this) as file_:
-        exec(file_.read(), dict(__file__=activate_this))
+activator = '/var/www/agro-python/bin/activate_this.py'  
+with open(activator) as f:
+    exec(f.read(), {'__file__': activator})
+#activate_this = '/var/www/agro-python/bin/activate_this.py'
+#
+#with open(activate_this) as file_:
+#       exec(file_.read(), dict(__file__=activate_this))
 # Tell mod_wsgi the root dir of the app        
 import sys
 sys.path.insert(0, '/var/www/AgroClimatic-Monitor/')
