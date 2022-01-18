8f06b1b7ecb97f6c24441a1771612ae6c79d2acd
cemachelen <h.l.burns@leeds.ac.uk>
2022-01-18 10:56:51 +0000

    :beetle: bug hunting for h5 missing cities

--- each_h5.py
+++ each_h5.py
@@ -1,11 +1,11 @@
 '''
 Process individual hdf5 FILES
 
-CSSP brazil 
+CSSP brazil
 written by d.ellis@leeds.ac.uk for CEMAC
 '''
 
-import geopandas as gpd  
+import geopandas as gpd
 import rasterio,h5py,glob,time
 from rasterio import MemoryFile
 from rasterio.mask import mask,raster_geometry_mask
@@ -18,13 +18,14 @@ import os,warnings,sys
 import dataconvert as dc
 import config as cf
 
-from params import cat_lims,colours 
+from params import cat_lims,colours
 warnings.filterwarnings('ignore', category=RuntimeWarning)
 
 '''
 Load Shapefiles
 '''
 brazil = gpd.read_file(cf.DATA+'shapefile/BR_borders/BR_MUN_WGS84.shp')
+print(cf.DATA+'shapefile/BR_borders/BR_MUN_WGS84.shp')
 breakearly = True
 
 
@@ -34,35 +35,35 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
     ----------------------------------------
     Indicator Function
     ----------------------------------------
-    i_kind :: str - indicator type 
+    i_kind :: str - indicator type
     indicator_files :: list - filelocations
     dataloc :: str - data Location
     imageloc :: str - image location for saving
     '''
-    
+    print(i_kind,FILES,dataloc,imageloc)
     print(i_kind, imageloc,dataloc)
-    
+
     thisfile = '%sdata_%s.h5'%(dataloc,i_kind)
     if os.path.exists(thisfile):
         basetime = os.path.getmtime(thisfile)
     else:
         basetime=-0
-    
+
     try:
         indicator = h5py.File( thisfile , 'a')
-        
+
     except:
             # try closing all instances of the file first
-            basetime = -1 
+            basetime = -1
             os.system('rm '+ thisfile )
             # input('deleting ctrl-c to cancel, enter to continue')
             # h5file = h5py.File( datafile, 'w')
             print('FAILED retrying')
             indicator = h5py.File( thisfile , 'a')
             None
-    
-    
-    
+
+
+
     FILES = glob.glob(FILES)
     nfiles = len(FILES)
     print('Number of %s files'%i_kind, nfiles)
@@ -73,22 +74,22 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
     CBAR
     '''
     cat = cat_lims[i_kind]
-                
+
     # bins = [cat_lims[1][0]]
     # bins.extend(cat_lims[0])
-    
-    
+
+
     bins = [cat[1][0]]
     bins.extend(cat[0])
     bins.append(cat[1][1])
-    
+
     col = colours
-    
+
     if bins[-1]<bins[0]:
         print('REVERSEcolour', i_kind)
         bins = bins[::-1]
-        cols = colours[::-1]               
-                                    
+        cols = colours[::-1]
+
     # assert len(bins)-1 == len(colours)
     cmap = mpl.colors.ListedColormap(col)
     norm = mpl.colors.BoundaryNorm(boundaries=bins, ncolors=len(cmap.colors)-1 )
@@ -103,7 +104,7 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
                                     spacing='proportional',
                                     orientation='horizontal')
     cb2.set_label(i_kind)
-    plt.savefig(imageloc+i_kind+'.png',  transparent=True,bbox_inches='tight',pad_inches=0)    
+    plt.savefig(imageloc+i_kind+'.png',  transparent=True,bbox_inches='tight',pad_inches=0)
 
 
 
@@ -122,13 +123,14 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
         Iterate over each county for each file
         - less IO overhead this way
         '''
-        
+
         if basetime>os.path.getmtime(f):
             print('Skipping:',f ,basetime, os.path.getmtime(f) )
-            continue # this file is older than the combigned one - ignore. 
-        
+            continue # this file is older than the combigned one - ignore.
+
         data = rasterio.open(f)
         fname = f.split('/')[-1].replace('.tiff','').replace('.tif','').replace(i_kind+'_','')
+        print(fname)
         if i_kind == 'VHI' or i_kind == 'IIS3':
             fname = fname.split('_')
             fname = '%s-%s'%(fname[1],fname[0])
@@ -145,13 +147,13 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
         ###
         if not os.path.exists('%s%s_%s.png'%(imageloc,fname,i_kind)):
             dc.getpng(f,fname,i_kind,cmap,norm,imageloc)
-        
-        
+
+
         for shape in brazil.iterrows():
-            
+
             selection = shape[1].geometry
             sname = str(shape[1].GEOCODIGO)
-            
+
             name = shape[1].NOME
             if not name: continue
 
@@ -164,39 +166,39 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
             except ValueError: continue
 
 
-            # MASK based on max limit   
+            # MASK based on max limit
             if i_kind != 'IIS3':
                 masked = where(clipped_array<=data.meta['nodata'],nan,clipped_array)
             else:
                 masked = where(clipped_array>=data.meta['nodata'],nan,clipped_array)
-            
-        
+
+
             # try: nanmax(masked)
             # except RuntimeWarning: continue
             if not isfinite(masked.any()): continue
 
 
             '''## save data'''
-            try: 
+            try:
                 dset = indicator_sub[fname]
                 if breakearly:
                     break # skip all muncipalities as we have the data for this timestep file.
             except KeyError:
-                
+
                 ### create dataset
                 dset = indicator_sub.create_dataset(fname, masked.shape, dtype='float32')
                 dset[:] = masked
-                dset.attrs['transform']= clipped_transform       
+                dset.attrs['transform']= clipped_transform
                 dset.attrs['geocode']=shape[1].GEOCODIGO
                 dset.attrs['name']=name
-                
+
                 ### dataset
                 dset.attrs['max'] = nanmax(masked)
                 dset.attrs['min'] = nanmin(masked)
                 dset.attrs['median'] = nanmedian(masked)
                 dset.attrs['mean'] = nanmean(masked)
                 dset.attrs['std'] = nanstd(masked)
-                # 
+                #
                 # print('----',dset.attrs['min'],dset.attrs['max'])
         print ('(%d/%d) :: '%(counter+1,nfiles),fname, 'took %.2f minutes'%((time.time()-start)/60) )
 
