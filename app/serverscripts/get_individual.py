f49bd6fe5d8affc415c6b7386f55f3f4f52c0287
cemachelen <h.l.burns@leeds.ac.uk>
2022-01-20 11:59:00 +0000

    :beetle: find why json not getting all groups

--- app/serverscripts/get_individual.py
+++ app/serverscripts/get_individual.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+import json
+import h5py
+from serverscripts.config import *
+import pandas as pd
+import numpy as np
 from warnings import simplefilter
 simplefilter("ignore", FutureWarning)
-import numpy as np
-import pandas as pd
-import h5py,json,time
-from serverscripts.config import *
-#min norm, abnorm, mod, sev, ext exception (else) max 8
-cat_lims = {"VHI":[[40,30,20,12,6],[100,0]],### VHI is backwards
-            "spi_01":[[-.5,-.8,-1.3,-1.6,-2],[4,-4]],
-            "spi_03":[[-.5,-.8,-1.3,-1.6,-2],[4,-4]],
-            "spi_06":[[-.5,-.8,-1.3,-1.6,-2],[4,-4]],
-            "spi_12":[[-.5,-.8,-1.3,-1.6,-2],[4,-4]],
-            "SPI":[[-.5,-.8,-1.3,-1.6,-2],[4,-4]],
-            "RZSM":[[40,30,20,12,6],[100,0]],
-            "IIS3":[[6,5,4,3,2],[7,0]]
+# min norm, abnorm, mod, sev, ext exception (else) max 8
+cat_lims = {"VHI": [[40, 30, 20, 12, 6], [100, 0]],  # VHI is backwards
+            "spi_01": [[-.5, -.8, -1.3, -1.6, -2], [4, -4]],
+            "spi_03": [[-.5, -.8, -1.3, -1.6, -2], [4, -4]],
+            "spi_06": [[-.5, -.8, -1.3, -1.6, -2], [4, -4]],
+            "spi_12": [[-.5, -.8, -1.3, -1.6, -2], [4, -4]],
+            "SPI": [[-.5, -.8, -1.3, -1.6, -2], [4, -4]],
+            "RZSM": [[40, 30, 20, 12, 6], [100, 0]],
+            "IIS3": [[6, 5, 4, 3, 2], [7, 0]]
             }
 
-location = PROCESSED+'muncipalities/'
+location = PROCESSED + 'muncipalities/'
 
-polydata = pd.read_csv(PROCESSED+'geojson/poly.csv')
+polydata = pd.read_csv(PROCESSED + 'geojson/poly.csv')
 polydata.GEOCODIGO = polydata.GEOCODIGO.astype(str)
 
 
-
-def categorize(x,idn):
+def categorize(x, idn):
     lims = cat_lims[idn]
     arr = []
-    if lims[1][0]>lims[1][1]:
+    if lims[1][0] > lims[1][1]:
         for i in x:
             counter = 0
             last = True
@@ -38,7 +39,7 @@ def categorize(x,idn):
                     last = False
                     break
                 else:
-                    counter+=1
+                    counter += 1
     else:
         for i in x:
             counter = 0
@@ -49,59 +50,58 @@ def categorize(x,idn):
                     last = False
                     break
                 else:
-                    counter+=1
+                    counter += 1
 
-    if last: arr.append(counter)
+    if last:
+        arr.append(counter)
 
-    return arr,lims[1]
+    return arr, lims[1]
 
 
 '''
 Open HDF5 datafile
 '''
+
+
 def m_new(code):
 
     # code = code.replace('file_','').replace('.json')
     global polydata
-    print(polydata.GEOCODIGO.values,code,'\n\n\n\n\n\n\n\n')
+    print(polydata.GEOCODIGO.values, code, '\n\n\n\n\n\n\n\n')
 
     start = time.time()
-    jsn_grp={}
+    jsn_grp = {}
     for hf in h5locs:
-        indicate = hf.replace(PROCESSED+'data_','').replace('.h5','')
+        indicate = hf.replace(PROCESSED + 'data_', '').replace('.h5', '')
         h5file = h5py.File(hf, 'r')
-        jsn={}
+        jsn = {}
         try:
             selection = h5file[code]
-            dtstr = [i+'-01 00:00:00' for i in selection]
+            dtstr = [i + '-01 00:00:00' for i in selection]
         except Exception as e:
             print(e)
             continue
 
-
-
         df = pd.Series([selection[i].attrs['median'] for i in selection],
-                         index = pd.to_datetime(dtstr).to_period('M')
-                      )
-
+                       index=pd.to_datetime(dtstr).to_period('M')
+                       )
 
         try:
-            jsn['cat'],jsn['lim'] = categorize(df.values,indicate)
+            jsn['cat'], jsn['lim'] = categorize(df.values, indicate)
             jsn['catlims'] = cat_lims[indicate][0]
         except Exception as p:
-            print(p,code, indicate)
+            print(p, code, indicate)
             continue
 
         jsn['x'] = df.index.astype('str')
         jsn['y'] = df.values
 
-
-
-
         for i in jsn:
+            print(jsn[i])
             jsn[i] = list(jsn[i])
 
-        if jsn!={}:
+        if jsn != {}:
+            print(indicate)
             jsn_grp[indicate] = jsn
 
         h5file.close()
@@ -113,47 +113,41 @@ def m_new(code):
     jsn_grp['geox'] = poly[0]
     jsn_grp['geoy'] = poly[1]
 
-    jsn_grp['micro']=it['MICROREGIA'].values[0]
-    jsn_grp['macro']=it['MESOREGIAO'].values[0]
-    jsn_grp['id']=it['id'].values[0]
-
+    jsn_grp['micro'] = it['MICROREGIA'].values[0]
+    jsn_grp['macro'] = it['MESOREGIAO'].values[0]
+    jsn_grp['id'] = it['id'].values[0]
 
     jsn = {}
     yv = []
     maxl = 0
     for spi in jsn_grp:
-        if 'spi_' in  spi:
+        if 'spi_' in spi:
             spd = jsn_grp[spi]
             jsn['x'] = spd['x']
             yv.append(spd['y'])
             # jsn['cat'] = spd['cat']
             #jsn['lim'] = spd['lim']
             jsn['catlims'] = spd['catlims']
-            maxl = max(maxl,len(jsn['x']))
+            maxl = max(maxl, len(jsn['x']))
 
     # lazily expand array
     for i in range(len(yv)):
-        for j in range(maxl-len(yv[i])):
+        for j in range(maxl - len(yv[i])):
             yv[i].append(0)
 
-
-    if len(yv)>0:
+    if len(yv) > 0:
 
         jsn['y'] = list(np.array(yv).mean(axis=0))
-        jsn['cat'],jsn['lim']= categorize(jsn['y'],indicate)
+        jsn['cat'], jsn['lim'] = categorize(jsn['y'], indicate)
     else:
         jsn = {}
 
     jsn_grp['SPI'] = jsn
 
+    json.dump(jsn_grp, open(location + 'file_%s.json' % code, 'w'))
 
-
-
-    json.dump(jsn_grp, open(location+'file_%s.json'%code,'w') )
-
-    print (time.time()-start)
-    return True#jsn_grp
-
+    print(time.time() - start)
+    return True
 
     #
     #
