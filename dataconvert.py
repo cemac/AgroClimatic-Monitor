26d90e845588abe001c31ff1a1c86f02c175726e
cemachelen <h.l.burns@leeds.ac.uk>
2022-01-26 17:00:26 +0000

    :necktie: :fire: lint, remove excess code increase dpi and remove blur

--- dataconvert.py
+++ dataconvert.py
@@ -1,77 +1,45 @@
-
 import rasterio as rio
 from rasterio.plot import show
 from rasterio.warp import reproject, Resampling
-
 import numpy as np
-
 from matplotlib import pyplot as plt
 import matplotlib as mpl
-import os 
+import os
 
-projection = 'EPSG:3857'
-input_proj = 'EPSG:4326'
+#projection = 'EPSG:3857'
+#input_proj = 'EPSG:4326'
 
 
-bbox = [5.2842873,-33.8689056,-35.6341164,-73.9830625]
+bbox = [5.2842873, -33.8689056, -35.6341164, -73.9830625]
 
-def getpng(loc,name,what,cmap,norm,where ='./processed/plotdata/' ):
-    
-    if os.path.isfile('%s%s_%s.png'%(where,name,what)): return None # exists
-        
+
+def getpng(loc, name, what, cmap, norm, where='./processed/plotdata/'):
+
+    if os.path.isfile('%s%s_%s.png' % (where, name, what)):
+        return None  # exists
+
     plt.clf()
-    
     ra = rio.open(loc)
-    bounds  = ra.bounds
-
-    ratio = ra.width/ra.height
-
-    # show(ra.read(), cmap='viridis')
-    # plt.show()
-    my_dpi=70
-    #plt.figure(figsize=(ra.width/my_dpi, ra.height/my_dpi), dpi=my_dpi)
-
+    bounds = ra.bounds
+    ratio = ra.width / ra.height
+    my_dpi = 70
     width = 400
-    height = int(.85 * width * ratio )
-    
-    
-#     dst_shape = (width, height)
-#     destination = np.zeros(dst_shape, np.uint8)
-    
-    
-    #destination = rio.open('./temp.tiff','w')
-#     reproject(
-#         ra,
-#         destination,
-#         # src_transform=src_transform,
-#         src_crs={'init': input_proj},
-#         # dst_transform=dst_transform,
-#         dst_crs={'init': projection},
-#         resampling=Resampling.nearest)
-#     # 
-    
-    plt.figure(figsize=(width/my_dpi, height/my_dpi), dpi=my_dpi)
-
+    height = int(.85 * width * ratio)
+    plt.figure(figsize=(width / my_dpi, height / my_dpi), dpi=200)
     ax = plt.gca()
-
-    
-    show(ra, cmap=cmap,norm=norm,with_bounds=True, ax = ax)
-    #ra.read(1, masked=True)
-    # 
-    plt.xlim(bbox[-1],bbox[1])
-    plt.ylim(bbox[-2],bbox[0])
-    
+    show(ra, cmap=cmap, norm=norm, with_bounds=True, ax=ax)
+    plt.xlim(bbox[-1], bbox[1])
+    plt.ylim(bbox[-2], bbox[0])
     plt.axis('off')
     plt.tight_layout()
-    
     plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-    
-    plt.text(.95,.95, '-'.join(name.split('-')[::-1]), horizontalalignment='center',          verticalalignment='center', transform=ax.transAxes, c='black')
-
-    print('FILENAME ' , what)
-    plt.savefig('%s.png'%what, dpi=my_dpi, transparent=True,bbox_inches='tight',pad_inches=0)
-    #plt.show()
+    plt.text(.95, .95, '-'.join(name.split('-')[::-1]), horizontalalignment='center',
+             verticalalignment='center', transform=ax.transAxes, c='black')
+    print('FILENAME ', what)
+    plt.savefig('%s.png' % what, dpi=200,
+                transparent=True, bbox_inches='tight', pad_inches=0)
     plt.close()
-
     # blur and invert
-    os.system('convert %s.png -transparent whitesmoke -blur 1x3 %s%s_%s.png'%(what,where,name,what))
+    # remove -blur 1x3
+    os.system('convert %s.png -transparent whitesmoke %s%s_%s.png' %
+              (what, where, name, what))
--- each_h5.py
+++ each_h5.py
@@ -6,30 +6,34 @@ written by d.ellis@leeds.ac.uk for CEMAC
 '''
 
 import geopandas as gpd
-import rasterio,h5py,glob,time
+import rasterio
+import h5py
+import glob
+import time
 from rasterio import MemoryFile
-from rasterio.mask import mask,raster_geometry_mask
+from rasterio.mask import mask, raster_geometry_mask
 from shapely.geometry import mapping
 
-from numpy import ma,nan,where,nanmean,nanstd,nanmedian,nanmax,nanmin,isfinite
+from numpy import ma, nan, where, nanmean, nanstd, nanmedian, nanmax, nanmin, isfinite
 from matplotlib import pyplot as plt
 import matplotlib as mpl
-import os,warnings,sys
+import os
+import warnings
+import sys
 import dataconvert as dc
 import config as cf
 
-from params import cat_lims,colours
+from params import cat_lims, colours
 warnings.filterwarnings('ignore', category=RuntimeWarning)
 
 '''
 Load Shapefiles
 '''
-brazil = gpd.read_file(cf.DATA+'shapefile/BR_borders/BR_MUN_WGS84.shp')
+brazil = gpd.read_file(cf.DATA + 'shapefile/BR_borders/BR_MUN_WGS84.shp')
 breakearly = False
 
 
-
-def parsefiles(i_kind,FILES,dataloc,imageloc):
+def parsefiles(i_kind, FILES, dataloc, imageloc):
     '''
     ----------------------------------------
     Indicator Function
@@ -39,35 +43,31 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
     dataloc :: str - data Location
     imageloc :: str - image location for saving
     '''
-    print(i_kind,FILES,dataloc,imageloc)
-    print(i_kind, imageloc,dataloc)
+    print(i_kind, FILES, dataloc, imageloc)
+    print(i_kind, imageloc, dataloc)
 
-    thisfile = '%sdata_%s.h5'%(dataloc,i_kind)
+    thisfile = '%sdata_%s.h5' % (dataloc, i_kind)
     if os.path.exists(thisfile):
         basetime = os.path.getmtime(thisfile)
     else:
-        basetime=-0
+        basetime = -0
 
     try:
-        indicator = h5py.File( thisfile , 'a')
+        indicator = h5py.File(thisfile, 'a')
 
     except:
-            # try closing all instances of the file first
-            basetime = -1
-            os.system('rm '+ thisfile )
-            # input('deleting ctrl-c to cancel, enter to continue')
-            # h5file = h5py.File( datafile, 'w')
-            print('FAILED retrying')
-            indicator = h5py.File( thisfile , 'a')
-            None
-
-
+        # try closing all instances of the file first
+        basetime = -1
+        os.system('rm ' + thisfile)
+        # input('deleting ctrl-c to cancel, enter to continue')
+        # h5file = h5py.File( datafile, 'w')
+        print('FAILED retrying')
+        indicator = h5py.File(thisfile, 'a')
+        None
 
     FILES = glob.glob(FILES)
     nfiles = len(FILES)
-    print('Number of %s files'%i_kind, nfiles)
-
-
+    print('Number of %s files' % i_kind, nfiles)
 
     '''
     CBAR
@@ -77,76 +77,64 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
     # bins = [cat_lims[1][0]]
     # bins.extend(cat_lims[0])
 
-
     bins = [cat[1][0]]
     bins.extend(cat[0])
     bins.append(cat[1][1])
 
     col = colours
 
-    if bins[-1]<bins[0]:
+    if bins[-1] < bins[0]:
         print('REVERSEcolour', i_kind)
         bins = bins[::-1]
         cols = colours[::-1]
 
     # assert len(bins)-1 == len(colours)
     cmap = mpl.colors.ListedColormap(col)
-    norm = mpl.colors.BoundaryNorm(boundaries=bins, ncolors=len(cmap.colors)-1 )
+    norm = mpl.colors.BoundaryNorm(
+        boundaries=bins, ncolors=len(cmap.colors) - 1)
 
     fig, ax = plt.subplots(figsize=(6, 1))
     fig.subplots_adjust(bottom=0.5)
     cb2 = mpl.colorbar.ColorbarBase(ax, cmap=cmap,
                                     norm=norm,
-                                    boundaries=  bins ,
+                                    boundaries=bins,
                                     extend='both',
                                     ticks=bins,
                                     spacing='proportional',
                                     orientation='horizontal')
     cb2.set_label(i_kind)
-    plt.savefig(imageloc+i_kind+'.png',  transparent=True,bbox_inches='tight',pad_inches=0)
+    plt.savefig(imageloc + i_kind + '.png',  transparent=True, bbox_inches='tight',
+                pad_inches=0)
 
-
-
-
-
-
-
-
-
-
-
-
-    for counter,f in enumerate(FILES):
+    for counter, f in enumerate(FILES):
         start = time.time()
         '''
         Iterate over each county for each file
         - less IO overhead this way
         '''
 
-        #if basetime>os.path.getmtime(f):
+        # if basetime>os.path.getmtime(f):
         #    print('Skipping:',f ,basetime, os.path.getmtime(f) )
         #    continue # this file is older than the combigned one - ignore.
 
         data = rasterio.open(f)
-        fname = f.split('/')[-1].replace('.tiff','').replace('.tif','').replace(i_kind+'_','')
+        fname = f.split('/')[-1].replace('.tiff',
+                                         '').replace('.tif', '').replace(i_kind + '_', '')
         print(fname)
         if i_kind == 'VHI' or i_kind == 'IIS3':
             fname = fname.split('_')
-            fname = '%s-%s'%(fname[1],fname[0])
+            fname = '%s-%s' % (fname[1], fname[0])
         elif i_kind == 'RZSM':
             fname = fname.split('_')
-            fname = '%s-%s'%(fname[0],fname[1])
+            fname = '%s-%s' % (fname[0], fname[1])
         else:
-            fname = '%s-%s'%(fname[0:4],fname[4:])
-
-
+            fname = '%s-%s' % (fname[0:4], fname[4:])
 
         ###
-        ## DRAW
+        # DRAW
         ###
-        if not os.path.exists('%s%s_%s.png'%(imageloc,fname,i_kind)):
-            dc.getpng(f,fname,i_kind,cmap,norm,imageloc)
-
+        if not os.path.exists('%s%s_%s.png' % (imageloc, fname, i_kind)):
+            dc.getpng(f, fname, i_kind, cmap, norm, imageloc)
 
         for shape in brazil.iterrows():
 
@@ -154,44 +142,52 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
             sname = str(shape[1].GEOCODIGO)
             print(sname)
             name = shape[1].NOME
-            if not name: continue
+            if not name:
+                continue
 
             '''## subgroup'''
-            try: indicator_sub = indicator[sname]
-            except KeyError: indicator_sub = indicator.create_group(sname)
+            try:
+                indicator_sub = indicator[sname]
+            except KeyError:
+                indicator_sub = indicator.create_group(sname)
 
-            ## clip
-            try:clipped_array, clipped_transform = mask(data, [mapping(selection)], crop=True)
-            except ValueError: continue
-
+            # clip
+            try:
+                clipped_array, clipped_transform = mask(
+                    data, [mapping(selection)], crop=True)
+            except ValueError:
+                continue
 
             # MASK based on max limit
             if i_kind != 'IIS3':
-                masked = where(clipped_array<=data.meta['nodata'],nan,clipped_array)
+                masked = where(clipped_array <=
+                               data.meta['nodata'], nan, clipped_array)
             else:
-                masked = where(clipped_array>=data.meta['nodata'],nan,clipped_array)
-
+                masked = where(clipped_array >=
+                               data.meta['nodata'], nan, clipped_array)
 
             # try: nanmax(masked)
             # except RuntimeWarning: continue
-            if not isfinite(masked.any()): continue
-
+            if not isfinite(masked.any()):
+                continue
 
             '''## save data'''
             try:
                 dset = indicator_sub[fname]
                 if breakearly:
-                    break # skip all muncipalities as we have the data for this timestep file.
+                    # skip all muncipalities as we have the data for this timestep file.
+                    break
             except KeyError:
 
-                ### create dataset
-                dset = indicator_sub.create_dataset(fname, masked.shape, dtype='float32')
+                # create dataset
+                dset = indicator_sub.create_dataset(
+                    fname, masked.shape, dtype='float32')
                 dset[:] = masked
-                dset.attrs['transform']= clipped_transform
-                dset.attrs['geocode']=shape[1].GEOCODIGO
-                dset.attrs['name']=name
+                dset.attrs['transform'] = clipped_transform
+                dset.attrs['geocode'] = shape[1].GEOCODIGO
+                dset.attrs['name'] = name
 
-                ### dataset
+                # dataset
                 dset.attrs['max'] = nanmax(masked)
                 dset.attrs['min'] = nanmin(masked)
                 dset.attrs['median'] = nanmedian(masked)
@@ -199,14 +195,16 @@ def parsefiles(i_kind,FILES,dataloc,imageloc):
                 dset.attrs['std'] = nanstd(masked)
                 #
                 # print('----',dset.attrs['min'],dset.attrs['max'])
-        print ('(%d/%d) :: '%(counter+1,nfiles),fname, 'took %.2f minutes'%((time.time()-start)/60) )
+        print('(%d/%d) :: ' % (counter + 1, nfiles), fname,
+              'took %.2f minutes' % ((time.time() - start) / 60))
 
     '''
     Close the hdf5 file cleanly
     '''
     indicator.close()
-    return i_kind+' finished'
+    return i_kind + ' finished'
+
 
 if __name__ == '__main__':
-#     print(sys.argv)
+    #     print(sys.argv)
     parsefiles(*sys.argv[1:5])
