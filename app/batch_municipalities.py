9f0619148cdd7835585a36db2d1c079f45798260
cemachelen <h.l.burns@leeds.ac.uk>
2022-01-28 23:46:47 +0000

    :sparkles: batch process to stop timeouts

---
+++ app/batch_municipalities.py
@@ -0,0 +1,10 @@
+
+from serverscripts.config import *
+import pandas as pd
+from serverscripts.get_individual import m_new
+
+
+df = pd.read_csv(PROCESSED+'geojson/search.csv')
+for key in df.iterrows():
+    code = key[1].val
+    m_new(code)
