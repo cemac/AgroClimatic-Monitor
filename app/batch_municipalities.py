c3aa112a57c8e97551468fd035470633eeac02bc
cemachelen <h.l.burns@leeds.ac.uk>
2022-01-29 00:48:43 +0000

    :pencil2: run in pip env

--- app/batch_municipalities.py
+++ app/batch_municipalities.py
@@ -7,5 +7,7 @@ from serverscripts.get_individual import m_new
 codelist = pd.read_csv(PROCESSED+'geojson/search.csv')
 for key in codelist.iterrows():
     code = str(key[1].val)
-    m_new(code)
-    break
+    try:
+        m_new(code)
+    except PermissionError:
+        continue
