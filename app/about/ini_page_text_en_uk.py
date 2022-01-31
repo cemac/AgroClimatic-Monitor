777d6a15651539ad57fb8ab6d3e57b751d615559
cemachelen <h.l.burns@leeds.ac.uk>
2022-01-31 11:35:33 +0000

    :sparkles: add pdfs

--- app/about/ini_page_text_en_uk.py
+++ app/about/ini_page_text_en_uk.py
@@ -3,25 +3,33 @@
 
 ini_page_title = ''' Agroclimatic Monitor '''
 
-ini_page_textbox1 = ''' 
+ini_page_textbox1 = '''
 
 “A drought monitoring tool for Brazilian agriculture”
 
-Brazil is one of the largest producers of many agricultural commodities in the world. 
-Every year, the Brazilian farmers are struck by droughts or extreme weather events, 
-leading to economic and resource losses. These events are expected to be more recurrent 
-in the future and understanding the risks and potential adaptations are key to achieve 
-food security. Monitoring drought conditions is the first step for understanding it’s 
-impacts and mitigation strategies in the agricultural sector. This is a challenging 
-task in continental countries like Brazil, where a huge variety of ecosystems and 
+Brazil is one of the largest producers of many agricultural commodities in the world.
+Every year, the Brazilian farmers are struck by droughts or extreme weather events,
+leading to economic and resource losses. These events are expected to be more recurrent
+in the future and understanding the risks and potential adaptations are key to achieve
+food security. Monitoring drought conditions is the first step for understanding it’s
+impacts and mitigation strategies in the agricultural sector. This is a challenging
+task in continental countries like Brazil, where a huge variety of ecosystems and
 culture is found all over the country.
 
-This tool is aimed to help decision making at municipal level (e.g. Civil Defense) 
-and growers that can monitor the monthly drought conditions for their locations by 
-means of drought indices, derived from remote sensed data and in-ground network of 
-weather stations. A team of scientists and programmers from the University of Leeds 
-and Cemaden regularly update and maintain this web-tool, which was funded by the 
+This tool is aimed to help decision making at municipal level (e.g. Civil Defense)
+and growers that can monitor the monthly drought conditions for their locations by
+means of drought indices, derived from remote sensed data and in-ground network of
+weather stations. A team of scientists and programmers from the University of Leeds
+and Cemaden regularly update and maintain this web-tool, which was funded by the
 Met Office CSSP-Brazil programme. For more information please check our “About us” page.
+
+<h3>Infomation Pages</h3>
+ <ul>
+  <li>Project Presentation <a href="/data/pdfs/1_Project_Presentation_EN.pdf">download</a></li>
+  <li>Platform AgroClimatic Monitor<a href="/data/pdfs/2_Platform_Agroclimatic_Monitor_EN.pdf">download</a></li>
+  <li>Modelling <a href="/data/pdfs/4_Modelling_EN.pdf">download</a></li>
+</ul>
+
 
 <img src='/data/geojson/ini2.jpg' style="width:100%"></img>
 
@@ -30,5 +38,3 @@ Met Office CSSP-Brazil programme. For more information please check our “About
 #--------------------------#
 #--- Fig1 as background ---#
 #--------------------------#
-
-
