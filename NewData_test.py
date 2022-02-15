3c16f9c57459fd02984e51f8425dbb6ab8bfcb4f
cemachelen <h.l.burns@leeds.ac.uk>
2022-02-15 13:59:52 +0000

    :hammer_and_wrench: set up to test current fnames

--- NewData_test.py
+++ NewData_test.py
@@ -7,7 +7,7 @@ Main Data Script
 Run this to create a HDF5 file for each inidicator.
 
 
-indicators: VHI SPI01 SPI03 SPI06 SPI12 IIS3 RZSM
+indicators: VHI spi01 spi03 spi06 spi12 IIS3 RZSM
 
 searches for tif files <apphome>/uolstorage/Data/<indicator>/<indicator>_*.tif*"
 
@@ -16,7 +16,7 @@ puts files <apphome>/processed/ and <apphome>/processed/plotdata/
 written by d.ellis@leeds.ac.uk for CEMAC
 '''
 import PostProcess
-from each_h5 import parsefiles
+from filenames_test import parsefiles
 import os
 import multiprocessing
 #from params import indicators, iloc
@@ -28,18 +28,19 @@ os.system('mkdir ' + imageloc)
 Parse FILES
 '''
 
-Test new indicators and ilocs
-indicators = ['VHI','SPI01','SPI03','SPI06','SPI12','IIS3','RZSM']
+'''
+indicators = ['VHI','spi01','spi03','spi06','spi12','IIS3','RZSM']
 
 iloc={
 'VHI':     'VHI/VHI_*.tif',
-'SPI03':  'SPI03/SPI03_.tif',
-'SPI06':  'SPI06/SPI06_*.tif',
-'SPI01':  'SPI01/SPI01_*.tif',
-'SPI12':  'SPI12/SPI12_*.tif',
+'spi03':  'spi03/spi03_.tif',
+'spi06':  'spi06/spi06_*.tif',
+'spi01':  'spi01/spi01_*.tif',
+'spi12':  'spi12/spi12_*.tif',
 'IIS3':    'IIS3/IIS3_*.tif',
 'RZSM':    'RZSM/RZSM_*.tif'
 }
+'''
 # args from config file
 args = [(kind, STORAGE + iloc[kind], PROCESSED, imageloc,)
         for kind in indicators]
@@ -47,11 +48,4 @@ args = [(kind, STORAGE + iloc[kind], PROCESSED, imageloc,)
 # for each indicator run each_h5
 for item in args:
     cmd = ' '.join(['"%s"' % i for i in item])
-    os.system('python each_h5.py ' + cmd)
-
-# movies
-PostProcess.update()
-
-print('ALL UP TO DATE')
-# clean up
-os.system('rm *_*.png')
+    os.system('python filenames_test.py ' + cmd)
