341b4f43afc07dd2ca765ea97233f8cf3934f8dc
Helen Burns <h.l.burns@leeds.ac.uk>
2022-10-17 10:38:59 +0100

    :wrench: :snake: add python testing

---
+++ .github/workflows/test.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+import os
+import subprocess
+import pytest
+from pathlib import Path
+
+
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.join(TEST_DIR, "../../app")
+
+od.chdir(PARENT_DIR)
+
+pytest app.py
