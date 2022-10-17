60a170d7479650424a26a8eda8e27aa40d1f4bb1
Helen Burns <h.l.burns@leeds.ac.uk>
2022-10-17 12:22:07 +0100

    :snake: test app.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,16 +1,3 @@
 """
 
 """
-import os
-import subprocess
-import pytest
-from pathlib import Path
-
-
-
-TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-PARENT_DIR = os.path.join(TEST_DIR, "../../app")
-
-od.chdir(PARENT_DIR)
-
-pytest app.py
